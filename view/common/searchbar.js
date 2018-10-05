c8ae17ecaa01e869f542012054ce4c8fc319b949
xuke <228619770@qq.com>
2018-10-05 10:21:58 +0800

    search

---
+++ view/common/searchbar.js
@@ -0,0 +1,66 @@
+/**
+ * 组件!!!
+ * */
+
+import React from 'react';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+
+
+export default class SearchBar extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.inputContainer}>
+                    <TextInput style={styles.input} {...this.props}/>
+                </View>
+                <TouchableOpacity style={styles.btn}{...this.props}>
+                    <Text style={styles.search}>搜索</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+var styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        height: 44,
+        marginTop: 10
+    },
+
+    inputContainer: {
+        flex: 1,
+        marginLeft: 5
+    },
+    input: {
+        flex: 1,
+        height: 44,
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: "#ccc",
+        padding: 5
+    },
+    btn: {
+        width: 55,
+        height: 44,
+        marginLeft: 5,
+        marginRight: 5,
+        backgroundColor: "#23BEFF",
+        borderRadius: 4
+    },
+    search: {
+        flex: 1,
+        color: "#fff",
+        fontSize: 15,
+        fontWeight: "bold",
+        textAlign: "center",
+        lineHeight: 40
+    }
+
+});
+
+
+
